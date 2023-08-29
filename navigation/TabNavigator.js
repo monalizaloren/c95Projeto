815299af11e8efc7004ae84bdb45598608d73db2
Monaliza Loren <83433815+monalizaloren@users.noreply.github.com>
2023-08-29 10:51:33 -0300

    Update TabNavigator.js

--- navigation/TabNavigator.js
+++ navigation/TabNavigator.js
@@ -8,28 +8,7 @@ const Tab = createBottomTabNavigator();
 
 const BottomTabNavigator = () => {
     return (
-        <Tab.Navigator
-            screenOptions={({ route }) => ({
-                tabBarIcon: ({ focused, color, size }) => {
-                    let iconName;
-                    if (route.name === 'Feed') {
-                        iconName = focused
-                            ? 'book'
-                            : 'book-outline';
-                    } else if (route.name === 'CriarPost') {
-                        iconName = focused ? 'create' : 'create-outline';
-                    }
-                    return <Ionicons name={iconName} size={size} color={color} />;
-                },
-            })}
-            tabBarOptions={{
-                activeTintColor: 'tomato',
-                inactiveTintColor: 'gray',
-            }}
-        >
-            <Tab.Screen name="Feed" component={Feed} />
-            <Tab.Screen name="CriarPost" component={CreatePost} />
-        </Tab.Navigator>
+        //passo 1
     );
 }
 
